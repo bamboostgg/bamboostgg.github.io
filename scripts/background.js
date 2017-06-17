7b3e76407786cc209fb977538ef166520caab078
Edison Huff <edishuff@amazon.com>
2017-06-17 10:19:58 -0700

    Updates flowing background to take vertical tilt (beta) into account

--- scripts/background.js
+++ scripts/background.js
@@ -1,18 +1,19 @@
 function liquidBackground() {
   var canvas = document.getElementById('background');
-  var radius = Math.sqrt(
-    Math.pow(canvas.height, 2) +
-    Math.pow(canvas.width, 2)
-  ) / 2;
+  var radius = setRadius();
+
+  function setRadius() {
+    return Math.sqrt(
+      Math.pow(canvas.height * 1.5, 2) +
+      Math.pow(canvas.width * 1.5, 2)
+    ) / 2;
+  }
 
   function resize() { 
     var ctx = canvas.getContext('2d');
     canvas.height = ctx.canvas.clientHeight;  
     canvas.width  = ctx.canvas.clientWidth;
-    radius = Math.sqrt(
-      Math.pow(canvas.height, 2) +
-      Math.pow(canvas.width, 2)
-    ) / 2;
+    radius = setRadius()
   }
 
   function toRadians (angle) {
@@ -22,19 +23,27 @@ function liquidBackground() {
   function animate(e) {
     var ctx = canvas.getContext('2d');
 
-    var height = canvas.height;
-    var width  = canvas.width;
-    var theta  = toRadians(e.gamma);
+    var vUnit = canvas.height / 3;
+    var width = canvas.width;
+    var theta = toRadians(
+      e.gamma * e.beta /90
+      + e.alpha * (1- e.beta / 90)
+    );
     var tan = Math.tan(theta);
+    var top = vUnit + vUnit * (e.beta / 90);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
     ctx.beginPath();
     ctx.fillStyle = '#1D4350';
-    ctx.arc(width/2, height/2, radius, -theta, -(theta - Math.PI));
+    ctx.arc(width/2, top, radius, -theta, -(theta - Math.PI));
     ctx.fill();
   }
 
   resize();
+  animate({
+    gamma: 0,
+    beta: 45,
+  })
   window.ondeviceorientation = animate;
 }
