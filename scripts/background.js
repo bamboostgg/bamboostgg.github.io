fe225430b916b577d936bc483bcd12bf7f67cc3f
Edison Huff <edishuff@amazon.com>
2017-06-19 10:01:12 -0700

    Removes alpha from background tilt logic

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,11 @@
 function liquidBackground() {
   var canvas = document.getElementById('background');
   var radius = setRadius();
-  var alpha;
 
   function setRadius() {
     return Math.sqrt(
-      Math.pow(canvas.height * 1.5, 2) +
-      Math.pow(canvas.width * 1.5, 2)
+      Math.pow(canvas.height * 2, 2) +
+      Math.pow(canvas.width * 2, 2)
     ) / 2;
   }
 
@@ -22,20 +21,12 @@ function liquidBackground() {
   }
 
   function animate(e) {
-    if (!alpha) {
-      alpha = e.alpha;
-    }
-
     var ctx = canvas.getContext('2d');
 
-    var vUnit = canvas.height / 3;
     var width = canvas.width;
-    var theta = toRadians(
-      e.gamma * e.beta /90
-      + (alpha - e.alpha) * (1 - e.beta / 90)
-    );
+    var theta = toRadians(e.gamma * e.beta /90 * (1 - e.beta / 90));
     var tan = Math.tan(theta);
-    var top = vUnit + vUnit * (e.beta / 90);
+    var top = canvas.height * (e.beta / 90);
 
     ctx.fillStyle = '#1D4350';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
